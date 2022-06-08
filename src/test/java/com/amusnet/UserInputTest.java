17f462ea2077f3ee19e03f3b827c92d61dc0a752
Hristo Kostadinov <hristo.kostadinov@amusnet.com>
2022-06-08 13:02:25 +0300

    Refactor User Input Tests

--- src/test/java/com/amusnet/UserInputTest.java
+++ src/test/java/com/amusnet/UserInputTest.java
@@ -2,6 +2,8 @@ package com.amusnet;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -15,6 +17,7 @@ import java.util.Random;
 public class UserInputTest {
 
     private static final int RANDOM_STRING_MAX_LENGTH = 30;
+    private static final String INVALID_INPUT_SEED = "H1e2l3l4o5W6o7r8l9d";
     private static final String EXIT_STRING = "quit";
 
     private static final Properties PROPERTIES = new Properties();
@@ -31,17 +34,57 @@ public class UserInputTest {
     private static OutputStream newError;
     private static PrintStream newStandardErr;
 
+    @Nested
+    @DisplayName("Test invalid user input")
+    class InvalidInputTests {
+
+        @BeforeAll
+        static void setUpInvalidInput() {
+            String invalidLinesPlayedInput = generateInvalidInput(INVALID_INPUT_SEED);
+            String invalidBetAmountInput = generateInvalidInput(INVALID_INPUT_SEED);
+
+            String invalidInput = invalidLinesPlayedInput + System.lineSeparator() +
+                    invalidBetAmountInput + System.lineSeparator() + EXIT_STRING;
+
+            newStandardIn = new ByteArrayInputStream(invalidInput.getBytes(StandardCharsets.UTF_8));
+
+            newOutput = new ByteArrayOutputStream();
+            newStandardOut = new PrintStream(newOutput);
+
+            newError = new ByteArrayOutputStream();
+            newStandardErr = new PrintStream(newError);
+
+            System.setIn(newStandardIn);
+            System.setOut(newStandardOut);
+            System.setErr(newStandardErr);
+        }
+
+        @Test
+        void userGivesInvalidInput_ThrowExceptionAndLogError() {
+            Application.main(null);
+            assertThat(newError.toString()).isEqualTo("Invalid input!" + System.lineSeparator() + "Invalid input!" + System.lineSeparator());
+        }
+
+        private static String generateInvalidInput(String seed) {
+            Random rnd = new Random();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < RANDOM_STRING_MAX_LENGTH; i++)
+                sb.append(seed.charAt(rnd.nextInt(seed.length())));
+
+            return sb.toString();
+        }
+
+    }
+
     @BeforeAll
-    static void changeStandardInputAndOutput() {
+    static void loadProperties() {
         try {
             PROPERTIES.load(new FileInputStream("src/main/resources/game.properties"));
         } catch (IOException e) {
             log.error("Error loading properties file");
             throw new RuntimeException(e);
         }
-
-        String invalidInputSeed = "H1e2l3l4o5W6o7r8l9d";
-        Random rnd = new Random();
 
 //        int maxLinesPlayed = Integer.parseInt(PROPERTIES.getProperty("max_lines"));
 //        int maxBetAmount = Integer.parseInt(PROPERTIES.getProperty("max_bet_amount"));
@@ -50,34 +93,7 @@ public class UserInputTest {
 //                rnd.nextInt(maxLinesPlayed + 1), System.lineSeparator(), rnd.nextInt(maxBetAmount + 1),
 //                System.lineSeparator(), EXIT_STRING);
 
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
 
-        for (int i = 0; i < RANDOM_STRING_MAX_LENGTH; i++)
-            sb1.append(invalidInputSeed.charAt(rnd.nextInt(invalidInputSeed.length())));
-        for (int i = 0; i < RANDOM_STRING_MAX_LENGTH; i++)
-            sb2.append(invalidInputSeed.charAt(rnd.nextInt(invalidInputSeed.length())));
-
-        String invalidUserInput = String.format("%s%s%s%s%s", sb1, System.lineSeparator(), sb2,
-                System.lineSeparator(), EXIT_STRING);
-
-        newStandardIn = new ByteArrayInputStream(invalidUserInput.getBytes(StandardCharsets.UTF_8));
-
-        newOutput = new ByteArrayOutputStream();
-        newStandardOut = new PrintStream(newOutput);
-
-        newError = new ByteArrayOutputStream();
-        newStandardErr = new PrintStream(newError);
-
-        System.setIn(newStandardIn);
-        System.setOut(newStandardOut);
-        System.setErr(newStandardErr);
-    }
-
-    @Test
-    void userGivesInvalidInput_ThrowExceptionAndLogError() {
-        Application.main(null);
-        assertThat(newError.toString()).isEqualTo("Invalid input!" + System.lineSeparator() + "Invalid input!" + System.lineSeparator());
     }
 
 }
