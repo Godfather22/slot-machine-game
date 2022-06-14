e0d8c635e6d23e492186a93ccc664f51d5dfb174
Hristo Kostadinov <hristo.kostadinov@amusnet.com>
2022-06-14 16:18:51 +0300

    Javadoc ErrorMessage.java

--- src/main/java/com/amusnet/util/ErrorMessage.java
+++ src/main/java/com/amusnet/util/ErrorMessage.java
@@ -4,6 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+/**
+ * A simple class for holding error message strings.
+ *
+ * @since 1.0
+ */
 @AllArgsConstructor
 @EqualsAndHashCode
 public class ErrorMessage {
