d40d3a0a0d5ad3f2f2c254bf1167e99c28d6e669
Hristo Kostadinov <hristo.kostadinov@amusnet.com>
2022-06-14 14:25:17 +0300

    Deprecate & javadoc Card.java

--- src/main/java/com/amusnet/game/Card.java
+++ src/main/java/com/amusnet/game/Card.java
@@ -5,6 +5,12 @@ import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * A simple class to represent a card of integer values.
+ *
+ * @deprecated No other type of card values are required for API other than integer.
+ */
+@Deprecated
 @Data
 @AllArgsConstructor
 @RequiredArgsConstructor
