24c5c6887a8eb411fee181921dbec9552802480f
Hristo Kostadinov <hristo.kostadinov@amusnet.com>
2022-06-15 16:58:10 +0300

    Add @SuppressWarnings("unused") to exception methods

--- src/main/java/com/amusnet/exception/ConfigurationInitializationException.java
+++ src/main/java/com/amusnet/exception/ConfigurationInitializationException.java
@@ -5,18 +5,22 @@ package com.amusnet.exception;
  * The XML containing configuration properties should be checked for valid content.
  */
 public class ConfigurationInitializationException extends Exception {
+    @SuppressWarnings("unused")
     public ConfigurationInitializationException() {
         super();
     }
 
+    @SuppressWarnings("unused")
     public ConfigurationInitializationException(String message) {
         super(message);
     }
 
+    @SuppressWarnings("unused")
     public ConfigurationInitializationException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    @SuppressWarnings("unused")
     public ConfigurationInitializationException(Throwable cause) {
         super(cause);
     }
