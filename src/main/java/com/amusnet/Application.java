2fbc43aac4e8d21d532137cf8f6c1d2cab525aaf
Hristo Kostadinov <hristo.kostadinov@amusnet.com>
2022-06-29 17:27:37 +0300

    Implement saving history to database

--- src/main/java/com/amusnet/Application.java
+++ src/main/java/com/amusnet/Application.java
@@ -1,35 +1,59 @@
 package com.amusnet;
 
+import com.amusnet.config.Jdbi;
 import com.amusnet.game.Game;
 import com.amusnet.util.ErrorMessages;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.statement.Update;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 import static com.amusnet.util.ErrorMessages.DefaultMessageTitles.*;
 
 public class Application {
 
-    public static final Game GAME = new Game();
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args) {
 
-        log.debug(System.lineSeparator() + GAME.getConfiguration().toString());
+        // TODO migrate to com.amusnet.game
+
+        final Game game = new Game();
+        final Logger log = LoggerFactory.getLogger(Application.class);
+
+        log.debug(System.lineSeparator() + game.getConfiguration().toString());
 
         ErrorMessages errorMessages = ErrorMessages.getInstance();
+        Jdbi j = Jdbi.getInstance();
+
+        LocalDateTime now = LocalDateTime.now();
+        String gameName = "game" + now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+        try (Handle handle = j.jdbi().open()) {
+            handle.execute("CREATE TABLE " + gameName + " (" +
+                    "  `turn` INT NOT NULL AUTO_INCREMENT," +
+                    "  `lines_played` INT NOT NULL," +
+                    "  `bet_amount` DECIMAL NOT NULL," +
+                    "  `total_win` DECIMAL NOT NULL," +
+                    "  PRIMARY KEY (`turn`)," +
+                    "  UNIQUE INDEX `turn_UNIQUE` (`turn` ASC) VISIBLE); ");
+
+            Update recordGame = handle.createUpdate("INSERT INTO `games` (started, name) VALUES (:timestamp, :name); ");
+            recordGame.bind("timestamp", now).bind("name", gameName);
+            recordGame.execute();
+        }
 
         Scanner sc = new Scanner(System.in);
-        final int maxLines = GAME.getConfiguration().getLineCount();
-        final double betLimit = GAME.getConfiguration().getBetLimit();
+        final int maxLines = game.getConfiguration().getLineCount();
+        final double betLimit = game.getConfiguration().getBetLimit();
 
-        final String exitCommand = GAME.getConfiguration().getExitCommand();
+        final String exitCommand = game.getConfiguration().getExitCommand();
 
         // main game loop
-        while (GAME.getGameState().getCurrentBalance() >= 0.0) {
+        while (game.getGameState().getCurrentBalance() >= 0.0) {
 
-            GAME.prompt();
+            game.prompt();
 
             String firstInput = sc.next();
             if (firstInput.equalsIgnoreCase(exitCommand))
@@ -77,22 +101,34 @@ public class Application {
                 }
             }
 
-            GAME.setupNextRound(linesInput, betInput);
-            GAME.getGameState().subtractFromBalance(betInput * linesInput);
+            game.setupNextRound(linesInput, betInput);
+            game.getGameState().subtractFromBalance(betInput * linesInput);
 
             // feedback
             System.out.printf("%s\t%s%nBalance: %s%n%n%s%n",
-                    GAME.getGameRound().getLinesPlayed(),
-                    GAME.getConfiguration().getCurrencyFormat().format(GAME.getGameRound().getBetAmount()),
-                    GAME.getConfiguration().getCurrencyFormat().format(GAME.getGameState().getCurrentBalance()),
-                    GAME.getGameRound().getReelScreen()
+                    game.getGameRound().getLinesPlayed(),
+                    game.getConfiguration().getCurrencyFormat().format(game.getGameRound().getBetAmount()),
+                    game.getConfiguration().getCurrencyFormat().format(game.getGameState().getCurrentBalance()),
+                    game.getGameRound().getReelScreen()
             );
 
-            GAME.playNextRound();
+            // side effect - prints necessary info to screen
+            double totalWin = game.playNextRound();
+
+            try (Handle handle = j.jdbi().open()) {
+                Update update = handle.createUpdate("INSERT INTO " + gameName + " " +
+                        "(`lines_played`, `bet_amount`, `total_win`)" +
+                        "VALUES " +
+                        "(:lines, :bet, :win); ");
+                update.bind("lines", game.getGameRound().getLinesPlayed())
+                        .bind("bet", game.getGameRound().getBetAmount())
+                        .bind("win", totalWin);
+                update.execute();
+            }
 
             System.out.println();
         }
-        System.out.println("Game over");
+        System.out.println("game over");
     }
 
 }
