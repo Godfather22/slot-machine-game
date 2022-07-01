56953990f1706049391b4f5d235dc6b0839b07f5
Hristo Kostadinov <hristo.kostadinov@amusnet.com>
2022-07-01 10:36:06 +0300

    Refactor: Externalize DB connection info in .properties file

--- src/main/java/com/amusnet/config/DatabaseConnectionJdbi.java
+++ src/main/java/com/amusnet/config/DatabaseConnectionJdbi.java
@@ -1,14 +1,19 @@
 package com.amusnet.config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class DatabaseConnectionJdbi {
 
     private static volatile DatabaseConnectionJdbi instance;
     private final org.jdbi.v3.core.Jdbi jdbi;
 
-    private DatabaseConnectionJdbi() {
+    private DatabaseConnectionJdbi() throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src/main/resources/db.properties"));
         jdbi = org.jdbi.v3.core.Jdbi.create
-                ("jdbc:mysql://localhost:3306/reel_game?createDatabaseIfNotExist=true",
-                        "root", "root");
+                (properties.getProperty("url"), properties);
     }
 
     public static DatabaseConnectionJdbi getInstance() {
@@ -16,8 +21,13 @@ public class DatabaseConnectionJdbi {
         if (result != null)
             return result;
         synchronized (DatabaseConnectionJdbi.class) {
-            if (instance == null)
-                instance = new DatabaseConnectionJdbi();
+            if (instance == null) {
+                try {
+                    instance = new DatabaseConnectionJdbi();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             return instance;
         }
     }
